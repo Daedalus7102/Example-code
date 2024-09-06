e6b7b143b7fd15b16ecf29344a52b975e19945f7
Jorge-Sanchezz <jsanchezencinas0804@gmail.com>
2024-09-06 14:19:57 -0600

    Talon SRX example added

--- src/main/java/frc/robot/subsystems/ExampleSubsystem.java
+++ src/main/java/frc/robot/subsystems/ExampleSubsystem.java
@@ -1,12 +1,14 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ExampleSubsystem extends SubsystemBase{
   //private final static Servo servo = new Servo(0);
   private final static VictorSPX cim = new VictorSPX(0);
+  private final static TalonSRX cim2 = new TalonSRX(1);
 
   public ExampleSubsystem() {
   }
@@ -17,6 +19,7 @@ public class ExampleSubsystem extends SubsystemBase{
   public void moveServo(double velocity) {
     //servo.setAngle(velocity);
     cim.set(ControlMode.PercentOutput, velocity);
+    cim2.set(ControlMode.PercentOutput, velocity);
   }
 
   public static double getPosition(){
